015abd0e0447837b978c538564ff49b372b331fe
hbdong <844199574@qq.com>
2017-08-31 16:13:57 +0800

    reboot

--- amazon/amazon/spiders/sales_ranking_spider.py
+++ amazon/amazon/spiders/sales_ranking_spider.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-import re
 import scrapy
 
 from pydispatch import dispatcher
@@ -14,7 +13,7 @@ from amazon.sql import RankingSql
 class SalesRankingSpider(scrapy.Spider):
     name = 'sales_ranking'
     custom_settings = {
-        'LOG_LEVEL': 'DEBUG',
+        'LOG_LEVEL': 'ERROR',
         'LOG_FILE': 'sales_ranking.json',
         'LOG_ENABLED': True,
         'LOG_STDOUT': True
@@ -30,14 +29,9 @@ class SalesRankingSpider(scrapy.Spider):
             yield scrapy.Request('https://www.amazon.com/dp/%s' % item['asin'], self.parse, meta={'item': item})
 
     def parse(self, response):
-        product_detail = response.xpath('//div/table').re(r'#[0-9,]+(?:.*)in .* \(.*[Ss]ee [Tt]op.*\)')
-        result = re.match(r'#([0-9,]+)(?:.*)in (.*) \(.*[Ss]ee [Tt]op.*\)', response.xpath('//body').extract()[0])
-        print('1', response.xpath('//body').extract()[0])
+        product_detail = response.xpath('//div/table').re(r'#[0-9,]+(?:.*)in.*\(.*[Ss]ee [Tt]op.*\)')
         if len(product_detail) == 0:
-            rankid_detail = response.css('table #SalesRank').re(r'.*#[0-9,]+(?:.*)in.*\(.*[Ss]ee [Tt]op.*\)')
-            print('2', rankid_detail)
-        else:
-            pass
+            product_detail = response.css('div #SalesRank').re(r'#[0-9,]+(?:.*)in.*\(.*[Ss]ee [Tt]op.*\)')
         if len(product_detail) != 0:
             item = SalesRankingItem()
             key_rank_str = product_detail[0]
@@ -45,12 +39,9 @@ class SalesRankingSpider(scrapy.Spider):
             item['rank'] = Helper.get_num_split_comma(key_rank_tuple[0])
             item['classify'] = key_rank_tuple[1]
             item['sk_id'] = response.meta['item']['id']
-            print(item)
+            yield item
         else:
-            print('catch asin[%s] sales ranking error' % response.meta['item']['asin'])
             raise Exception('catch asin[%s] sales ranking error' % response.meta['item']['asin'])
 
     def load_asin(self):
         self.items = RankingSql.fetch_sales_ranking()
-
-
